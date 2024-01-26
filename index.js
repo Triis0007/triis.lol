e867b552aabd6a6ecb9e3fade547d5059fe4d876
Triis0007 <69483583+Triis0007@users.noreply.github.com>
2024-01-26 00:41:55 -0300

    Agora foi

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
 const app = express();
 
-app.use(express.static('views'))
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 
 app.get('/', (req, res) => {
@@ -9,10 +17,10 @@ app.get('/', (req, res) => {
 });
 
 
-app.use(express.static("./public"), (_, res) => {
-  res.status(404)
-  res.send("404")
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
+
 
 app.listen(80, () => {
   console.clear()

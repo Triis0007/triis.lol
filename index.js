b11d223eca7004a0ad644b8a768ad240c537c57f
Triis0007 <69483583+Triis0007@users.noreply.github.com>
2024-02-24 20:56:52 -0300

    Added meta tags in index.html

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'views')));
 
 
 app.get('/', (req, res) => {
-  res.send('index');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 
